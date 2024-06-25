8f74dcd31150aa1094ab392d9fa1a9f3839d268d
Hong5743 <136396772+Hong5743@users.noreply.github.com>
2024-06-25 21:09:19 +0900

    Update game.js

--- game/js/game.js
+++ game/js/game.js
@@ -5,3 +5,7 @@ const windowEvent = () => {              // 윈도우 이벤트 함수(키, 마�
 const init = () => {                     // 프로그램 시작에 필요한 함수
   windowEvent();
 }
+
+window.onload = () => {                  // 파일이 실행되고 실행되는 함수
+  init();
+}  
